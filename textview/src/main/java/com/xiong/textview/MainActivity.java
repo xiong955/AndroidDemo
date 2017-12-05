1448e5d3c07a575beb9ab02deb88b968b706098c
xiongjunjie <xiongjunjie@funtsui.com>
2017-12-05 09:51:21 +0800

    add ui

--- textview/src/main/java/com/xiong/textview/MainActivity.java
+++ textview/src/main/java/com/xiong/textview/MainActivity.java
@@ -2,8 +2,12 @@ package com.xiong.textview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.xiong.textview.widget.VerticalRollingTextView;
 
@@ -12,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private VerticalRollingTextView tv;
     private Button btn1;
     private Button btn2;
+
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 tv.reset();
             }
         });
+
+
+
+
+        tv1 = findViewById(R.id.tv1);
+        ImageSpan span = new ImageSpan(this, R.mipmap.ic_launcher);//加载图片的资源
+        SpannableString spanStr = new SpannableString("icon");//不想要这个字符串可以试试空字符串
+        spanStr.setSpan(span, 0, spanStr.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv1.setText(spanStr);
+        tv1.append("asdasdasdasdasdsadasdasdasdasdasdasdasdasdasdasdasdsadasdasd");
     }
 }
---
+++ ui/src/main/java/com/xiong/ui/widget/StatusLayout.java
@@ -0,0 +1,33 @@
+package com.xiong.ui.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * @author: xiong
+ * @time: 2017/11/30
+ * @说明:
+ */
+
+public class StatusLayout extends FrameLayout {
+
+    private static final String TAG = "StatusLayout";
+
+    public StatusLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public StatusLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StatusLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
